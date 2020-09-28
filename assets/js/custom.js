fb2803e10c13643328278faff8c506b6a25ed656
Rajab Raza <mohammadrajabraza@gmail.com>
2020-09-28 21:39:28 +0500

    FEA: Banner text animation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -85,6 +85,56 @@
 		event.preventDefault();
 		}
 	});
+	// Banner Text animation
+	consoleText(['Book Your Desired Service'], 'text',['lightblue']);
+	function consoleText(words, id, colors) {
+		if (colors === undefined) colors = ['#fff'];
+		var visible = true;
+		var con = document.getElementById('console');
+		var letterCount = 1;
+		var x = 1;
+		var waiting = false;
+		var target = document.getElementById(id)
+		target.setAttribute('style', 'color:' + colors[0])
+		window.setInterval(function() {
+	  
+		  if (letterCount === 0 && waiting === false) {
+			waiting = true;
+			target.innerHTML = words[0].substring(0, letterCount)
+			window.setTimeout(function() {
+			  var usedColor = colors.shift();
+			  colors.push(usedColor);
+			  var usedWord = words.shift();
+			  words.push(usedWord);
+			  x = 1;
+			  target.setAttribute('style', 'color:' + colors[0])
+			  letterCount += x;
+			  waiting = false;
+			}, 1000)
+		  } else if (letterCount === words[0].length + 1 && waiting === false) {
+			waiting = true;
+			window.setTimeout(function() {
+			  x = -1;
+			  letterCount += x;
+			  waiting = false;
+			}, 1000)
+		  } else if (waiting === false) {
+			target.innerHTML = words[0].substring(0, letterCount)
+			letterCount += x;
+		  }
+		}, 120)
+		window.setInterval(function() {
+		  if (visible === true) {
+			con.className = 'console-underscore hidden'
+			visible = false;
+	  
+		  } else {
+			con.className = 'console-underscore'
+	  
+			visible = true;
+		  }
+		}, 400)
+	  }
 	function callbackFunction (resp) {
 		if (resp.result === "success") {
 		formSuccessSub();
@@ -123,7 +173,7 @@
 
 	// PRELOADER
 	jQuery(window).on('load',function(){
-		jQuery(".loader").fadeOut(500);
+		jQuery(".loader").fadeOut(1000);
 	});
 
 	// Wow JS
